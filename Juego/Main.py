d23fd336bd77358a7587a672bb0f0450d3735d5f
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-11-20 12:11:50 -0300

    Main

---
+++ Juego/Main.py
@@ -0,0 +1,25 @@
+import random
+
+def obtener_lista_niveles() -> dict:
+    with open("problemas.csv", "r", encoding = "utf8") as problemas:
+        problemas.readline()
+        keys = {
+            "1": [[], []],
+            "2": [[], []],
+            "3": [[], []],
+            "4": [[], []],
+            "5": [[], []]
+        }
+
+        for linea in problemas:
+            dificultad, ecuacion, resultado = linea.split(",")
+            dificultad = dificultad.strip()
+            resultado = int(resultado.strip())
+            keys[dificultad][0].append(ecuacion.strip())
+            keys[dificultad][1].append(resultado)
+    return keys
+
+#funcion | dentro del nivel seleccionar un ejercicio random
+
+resultado = obtener_lista_niveles()
+print(resultado)
